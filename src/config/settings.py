c799109e5c0d5cd61f01cdc886adda13fd17346b
Facundo Moreyra <fmoreyra2011@gmail.com>
2025-10-18 19:44:59 -0300

    fix: Switch to Django static file serving for production
    Temporarily switch from WhiteNoise to Django's static file serving
    to debug the 404 issue. Add static() URL patterns for production.

--- src/config/settings.py
+++ src/config/settings.py
@@ -241,7 +241,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = ["/public", os.path.join(BASE_DIR, "..", "public")]
 STATIC_ROOT = "/public_collected"
-STATICFILES_STORAGE = "whitenoise.storage.StaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Django Debug Toolbar
 # https://django-debug-toolbar.readthedocs.io/
